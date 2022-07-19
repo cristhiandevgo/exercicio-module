23cf2c21f20da3af084975cbc5707c902927eb94
cristhiandevgo <ivancristhian@hotmail.com>
2022-07-19 11:01:07 -0300

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Maria", 20));
+console.log(mostraCidade("Maria", "São Paulo"));
+console.log(mostraHobby("Maria", "Futebol"));
